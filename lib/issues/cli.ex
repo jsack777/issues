4cedf0604365db1e3afea9f11ab83ea193ea3002
jsackett <jsackett@comverge.com>
2016-02-01 13:11:05 -0700

    process cli

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -7,7 +7,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+     |> parse_args
+     |> process
   end
 
   @doc """
@@ -28,5 +30,16 @@ defmodule Issues.CLI do
 
       _ -> :help
     end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GitubIssues.fetch(user, project)
   end
 end
